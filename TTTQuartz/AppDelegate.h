834bd0d38cc0a67b3bec636986728ffe4d03afff
Kent Miller <kpmiller@apple.com>
2016-10-12 15:48:14 -0500

    Initial Commit

---
+++ TTTQuartz/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TTTQuartz
+//
+//  Created by Kent Miller on 10/12/16.
+//  Copyright © 2016 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TTTQuartz/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TTTQuartz
+//
+//  Created by Kent Miller on 10/12/16.
+//  Copyright © 2016 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
